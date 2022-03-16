704853462a55f3efdb11d6d3cb01004e907f6fb2
alfa212 <angento@gmail.com>
2022-03-16 09:56:55 +0300

    added diet buttons

--- second_step.py
+++ second_step.py
@@ -11,9 +11,21 @@ def user_registration(token):
     @bot.message_handler(commands=['start', 'help'])
     def send_welcome(message):
         markup = types.InlineKeyboardMarkup()
-        button1 = types.InlineKeyboardButton("Оформить подписку", url='https://test.test')
-        markup.add(button1)
+        start_subs = types.InlineKeyboardButton("Оформить подписку", callback_data='start_subs')
+        markup.add(start_subs)
         bot.send_message(message.chat.id, "Для оформления подписки нажмите на кнопку", reply_markup=markup)
+
+    @bot.callback_query_handler(func=lambda call: True)
+    def answer(call):
+        markup = types.InlineKeyboardMarkup()
+        if call.message:
+            if call.data == "start_subs":
+                classic_diet = types.InlineKeyboardButton("Классическая", callback_data='classic_diet')
+                vegan_diet = types.InlineKeyboardButton("Вегетарианская", callback_data='vegan_diet')
+                keto_diet = types.InlineKeyboardButton("Кето", callback_data='keto_diet')
+                markup.add(classic_diet, vegan_diet, keto_diet)
+                bot.send_message(call.message.chat.id, "Выберите тип диеты:", reply_markup=markup)
+                print('OK')
 
     bot.infinity_polling()
 
