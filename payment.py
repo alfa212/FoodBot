15236e54467ceb9ff7844244d029f3a98f6765b7
clownkill <alehnovich90@yandex.ru>
2022-03-17 15:41:56 +0300

    Fix bot payment processing

--- payment.py
+++ payment.py
@@ -27,31 +27,41 @@ def user_payment(token, pay_token):
                 provider_token=pay_token,
                 currency='rub',
                 prices=[price],
-                start_parameter='1-month-subsription-example',
+                start_parameter='subsription-example',
                 invoice_payload='some-invoice-payload-for-our-internal-use'
             )
 
 
-        @bot.pre_checkout_query_handler(func=lambda query: True)
-        def process_pre_checkout_query(pre_checkout_query):
-            bot.answer_pre_checkout_query(pre_checkout_query.id, ok=True)
+    @bot.pre_checkout_query_handler(func=lambda query: True)
+    def checkout(pre_checkout_query):
+        bot.answer_pre_checkout_query(
+            pre_checkout_query.id,
+            ok=True,
+            error_message='Инопланетяне пытались угнать ваш CVV, но мы защитили ваши данные, '
+                          'поппробуйте оплатить через пару минут.'
+        )
 
 
-        @bot.message_handler(content_types=['SUCCESSFUL_PAYMENT'])
-        def process_successful_payment(message):
-            print('successful_payment:')
-            pmnt = message.successfull_payment.to_python()
-            for key, val in pmnt.items():
-                print(f'{key} = {val}')
+    @bot.message_handler(content_types=['successful_payment'])
+    def got_payment(message):
+        pay_message = f'Спасибо за оплату! Вы оплатили ' \
+                      f'{message.successful_payment.total_amount / 100} ' \
+                      f'{message.successful_payment.currency} ' \
+                      f'за подписку на индивидуальное меню'
 
-            bot.send_message(message.chat.id,
-                             f'successfull_payment: '
-                             f'{message.successfull_payment.total_amount // 100} '
-                             f'{message.successfull_payment.currency}'
-                             )
+        print('successful_payment:')
+        print(f'От пользователя: {message.from_user.id}\n'
+              f'поступил платеж с id: {message.successful_payment.telegram_payment_charge_id} '
+              f'в размере: {message.successful_payment.total_amount / 100} {message.successful_payment.currency}')
+
+        bot.send_message(
+            message.chat.id,
+            pay_message,
+            parse_mode='Markdown'
+        )
 
 
-    bot.infinity_polling()
+    bot.infinity_polling(skip_pending=True)
 
 
 
