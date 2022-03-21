ef6df0445890198c682c71e74475d0175f97b281
abarzenok <69277070+abarzenok@users.noreply.github.com>
2022-03-21 18:53:08 +0300

    Simplify registration and dump user json to file

--- second_step.py
+++ second_step.py
@@ -22,24 +22,17 @@ def user_registration(token, users):
         last_name = message.from_user.last_name if message.from_user.last_name else ''
         full_name = f'{first_name} {last_name}'
         msg = bot.reply_to(
-            message, f'Добрый день, {full_name}!\n'
+            message, f'Добрый день! Необходимо зарегистрироваться\n'
         )
-        # markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-        markup = types.InlineKeyboardMarkup()
-        user_name = types.InlineKeyboardButton(f'{full_name}', callback_data='user_name')
-        custom_name = types.InlineKeyboardButton('Другое имя', callback_data='custom_name')
-        markup.add(user_name, custom_name)
-        # name_choice = bot.reply_to(msg, 'Могу я дальше вас так называть или хотите ввести другое имя?', reply_markup=markup)
-        bot.reply_to(msg, 'Могу я дальше вас так называть или хотите ввести другое имя?',
-                                   reply_markup=markup)
+        name = bot.reply_to(msg, 'Введите имя: ')
+        bot.register_next_step_handler(name, process_name_step)
+
     def write_new_name_step(message):
         message = bot.reply_to(message, 'Введите имя: ')
         bot.register_next_step_handler(message, process_name_step)
 
     def process_name_step(message):
         try:
-            if message.data == 'user_name':
-                pass  # TODO
             users[f'{message.from_user.id}'] = {}  # User(name)
             users[f'{message.from_user.id}']['name'] = message.text
             msg = bot.reply_to(message, 'Введите ваш номер телефона: ')
@@ -52,19 +45,29 @@ def user_registration(token, users):
         try:
             phone_number = message.text
             users[f'{message.from_user.id}']['phone_number'] = phone_number
+            users[f'{message.from_user.id}']['subscriptions'] = []
+            users[f'{message.from_user.id}']['diet'] = None
+            users[f'{message.from_user.id}']['meals_number'] = None
+            users[f'{message.from_user.id}']['persons_number'] = None
+            users[f'{message.from_user.id}']['allergies'] = []
+            users[f'{message.from_user.id}']['favourite_dishes'] = []
+            users[f'{message.from_user.id}']['disliked_dishes'] = []
+
+            with open('users.json', mode='w', encoding='utf-8') as file:
+                json.dump(json.dumps(users), file)
+
             bot.send_message(message.chat.id, str(users[f'{message.from_user.id}']))
+
+
             markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-            markup.add('Оформить подписку')
-            msg = bot.reply_to(message, 'Оформите подписку', reply_markup = markup)
-            bot.register_next_step_handler(msg, subscribe_step)
+            markup = types.InlineKeyboardMarkup()
+            start_subs = types.InlineKeyboardButton("Оформить подписку", callback_data='start_subs')
+            markup.add(start_subs)
+            bot.send_message(message.chat.id, "Для оформления подписки нажмите на кнопку", reply_markup=markup)
         except Exception as e:
+            print(e)
             bot.reply_to(message, 'Произошла ошибка, попробуйте зарегистрироваться заново, отправив "/start"')
 
-    def subscribe_step(message):
-        markup = types.InlineKeyboardMarkup()
-        start_subs = types.InlineKeyboardButton("Оформить подписку", callback_data='start_subs')
-        markup.add(start_subs)
-        bot.send_message(message.chat.id, "Для оформления подписки нажмите на кнопку", reply_markup=markup)
 
     @bot.callback_query_handler(func=lambda call: True)
     def answer(call):
